f8ef724d39da9b8250e0ae370622c8624b5e5a80
djupp <mdk2134@columbia.edu>
2017-01-22 23:31:21 -0500

    Move to hash completed, print function reimplemented

--- TruthTable.rb
+++ TruthTable.rb
@@ -1,5 +1,5 @@
 require 'matrix'
-require 'Logic'
+require './Logic'
 
 CAPITAL_ALPHA = (1..26).to_a.zip(("A".."Z").to_a).to_h
 STRING2BOOL = {'T'=> true, 'F'=> false}
@@ -7,21 +7,26 @@ BOOL2STRING = {false => 'F', true => 'T'}
 
 class TruthTable
 
+  # Should be a hash-like structure that has the Atoms as keys and the truth values as value
+
   def initialize(n_variables, variable_names=CAPITAL_ALPHA)
 
     @n = n_variables
     @variables = create_variable_array(@n, variable_names)
-
-    @tt = create_initial_matrix(@n)
+    puts @variables
+    @tt = create_initial_hash(@variables)
+    print_to_console @tt
 
   end
 
-  def print_to_console m
-    @variables.each{|v| print "#{v}\t"}
+  def create_variable_array(n, names)
+    return (1..n).map{|x| Variable.new(names[x])}
   end
 
-  def to_markdown
-
+  def create_initial_hash vars
+    hash = {}
+    vars.each_with_index{|v, idx| hash[v] = [true, false].repeated_permutation(vars.length).to_a.map{|x| x[idx]}}
+    return hash
   end
 
   # Adds a Well-Formed Formula to the table
@@ -30,12 +35,27 @@ class TruthTable
 
   end
 
-  def create_variable_array(n, names)
-    return (1..n).map{|x| names[x]}
+  def get_row tt, row
+    tt.keys.map{|col| tt[col][row]}
   end
 
-  def create_initial_matrix n_columns
+  # Printing etc.
+   
+  def print_to_console tt
+    tt.keys.each{|v| print "#{v.to_s}\t"}
+    puts "\n"
+    puts "———————" * tt.keys.length
+    0.upto(tt.values[0].length - 1) do |row_index|
+      get_row(tt, row_index).each{|x| print "#{x}\t"}
+      print "\n"
+    end
   end
+
+  def to_markdown
+
+  end
+
+
 end
 
 puts ARGV
